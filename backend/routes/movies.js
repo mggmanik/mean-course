bb339f9b63cfc5d96afe50daefd103f9e0d6270f
Manik <mggmanik@gmail.com>
2018-12-17 17:27:54 +0530

    code cleanup backend v2

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,34 +1,12 @@
 const express = require("express");
-const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
 const MovieController = require("../controllers/movie");
+const extractFile = require("../middleware/file");
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
+router.post("", checkAuth, extractFile, MovieController.createMovie);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid Mime Type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const extension = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + extension);
-  }
-});
-
-router.post("", checkAuth, multer({storage: storage}).single("image"), MovieController.createMovie);
-
-router.put("/:id", checkAuth, multer({storage: storage}).single("image"), MovieController.updateMovie);
+router.put("/:id", checkAuth, extractFile, MovieController.updateMovie);
 
 router.get("", MovieController.getMovies);
 
